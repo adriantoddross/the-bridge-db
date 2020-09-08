43cc3d86b890a6a4af8646959567529327782467
Adrian Ross <adriantoddross@gmail.com>
2020-09-08 14:12:32 -0400

    RRegister schema with apollo

--- index.js
+++ index.js
@@ -41,10 +41,16 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine: {
+    reportSchema: true,
+    variant: "current",
+  },
+});
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-
