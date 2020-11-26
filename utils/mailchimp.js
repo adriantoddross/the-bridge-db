46c905571deb69a552ed5761678d7d9e257c350b
Adrian Ross <adriantoddross@gmail.com>
2020-11-25 23:57:09 -0500

    Add method to subscribe user

--- utils/mailchimp.js
+++ utils/mailchimp.js
@@ -1,4 +1,5 @@
 const mailchimp = require("@mailchimp/mailchimp_marketing");
+const md5 = require("md5");
 
 mailchimp.setConfig({
   apiKey: `${process.env.MAILCHIMP_API_KEY}`,
@@ -9,6 +10,17 @@ const mailchimpAPI = {
   init: async () => {
     const response = await mailchimp.ping.get();
     console.log(response);
+  },
+  subscribeUser: async (email) => {
+    const subscriberHash = md5(email.toLowerCase());
+    const response = await mailchimp.lists.setListMember(
+      `${process.env.MAILCHIMP_LIST_ID}`,
+      subscriberHash,
+      { email_address: email, status_if_new: "subscribed" }
+    );
+
+    const { status, email_address } = response;
+    console.log(`Email: ${email_address}, Status: ${status}`);
   },
 };
 
