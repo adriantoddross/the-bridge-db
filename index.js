fd6bf46c660009b225a43f7ac8860d810e4008c5
Adrian Ross <adriantoddross@gmail.com>
2020-11-30 23:02:34 -0500

    Allow cors & use whitelist

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const apollo = require("./apollo.config");
 const routes = require("./routes");
+const { corsOptions } = require("./utils");
 
-const app = express();
 const port = process.env.PORT || 4000;
+const app = express();
 
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-
+app.use(cors(corsOptions));
 app.use("/", routes);
 
 apollo.applyMiddleware({ app });
