ef06d2647ff611a9e054807bbc62a2c2861d8a2f
Adrian Ross <adriantoddross@gmail.com>
2020-11-30 23:26:50 -0500

    Refactor error handling

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,25 @@ const mailchimp = require("../utils/mailchimp");
 const router = express.Router();
 
 router.post("/subscribe", (req, res) => {
+  const { email } = req.body;
   const validEmailRegex = RegExp(
     /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
   );
 
-  if (req.body.email && req.body.email.match(validEmailRegex)) {
-    try {
+  if (!req.body) {
+    res.status(400).json({ error: "Request body is empty." });
+  } else if (!email) {
+    res
+      .status(400)
+      .json({ error: "E-mail address is missing in request body." });
+  } else {
+    if (email.match(validEmailRegex)) {
       mailchimp.subscribeUser(req.body.email).then((response) => {
         res.status(200).json(response);
       });
-    } catch (error) {
-      res.status(500).json({ error });
+    } else {
+      res.status(400).json({ error: "E-mail address is invalid." });
     }
-  } else {
-    res
-      .status(400)
-      .json({ error: "E-mail address is invalid or it wasn't provided." });
   }
 });
 
